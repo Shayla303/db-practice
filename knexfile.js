753a325e947d08c4ce50ac0fd09400c0df097c73
Shayla Richard <shaylarichard@hotmail.com>
2017-11-15 11:41:36 -0700

    first commit

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,14 @@
+var env = require('dotenv'). coonfig()
+
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/insertTest'
+  }
+},
+
+production: {
+  client:'pg',
+  connection:process.env.DATABASE_URL
   }
 }
